8edc64361f6e9189e29f43af29798b5b144e7ebf
lya lima <lyalima0328@gmail.com>
2024-06-21 12:26:37 -0300

    ajuste staticfiles

--- app/settings.py
+++ app/settings.py
@@ -123,9 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-]
+]'''
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
