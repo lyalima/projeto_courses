3921a176b4748f7b85cfb4a0b592953581304c2d
lya lima <lyalima0328@gmail.com>
2024-06-21 11:58:13 -0300

    ajuste email address

--- utils/send_verification_email.py
+++ utils/send_verification_email.py
@@ -1,10 +1,10 @@
 from django.core.mail import send_mail
-import os
+from decouple import config
 
 
 def send_verification_email(user):
     subject = 'Verifique seu endereço de email'
     message = f'Use este código para verificar seu email: {user.verification_code}'
-    from_email = os.environ['EMAIL_ADDRESS']
+    from_email = config('EMAIL_ADDRESS')
     recipient_list = [user.email]
     send_mail(subject, message, from_email, recipient_list)
